52b28838b4062198e227e62adfb8c2cf9cdf39b7
Maze-o <ilsame9970@naver.com>
2024-09-10 15:15:16 +0900

    .

---
+++ src/Q/CodingTest1.java
@@ -0,0 +1,12 @@
+package Q;
+
+public class CodingTest1 {
+    public static void main(String[] args) {
+        int[] num_list;
+        int n;
+        int[] answer;
+        if(num_list.length % n == 0){
+        answer = new int[num_list.length /n];
+        }
+    }
+}
